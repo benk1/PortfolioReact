8998cf5a72c3a07990e208289f6165370730182c
benk1 <kakengi_bernard@hotmail.com>
2019-03-18 15:01:17 +0200

    Updates

---
+++ precache-manifest.bb22a9da7911e148d733d8acc3358e17.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/PortfolioReact/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4f825368a8c60a958a02",
+    "url": "/PortfolioReact/static/css/main.5a9752ba.chunk.css"
+  },
+  {
+    "revision": "1ac6a8f9f936383437a7",
+    "url": "/PortfolioReact/static/js/runtime~main.1ac6a8f9.js"
+  },
+  {
+    "revision": "415020d658652b52964593223be180c0",
+    "url": "/PortfolioReact/static/media/profile_pic.415020d6.jpg"
+  },
+  {
+    "revision": "d1375858a641f010f588",
+    "url": "/PortfolioReact/static/js/2.d1375858.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/PortfolioReact/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "4f825368a8c60a958a02",
+    "url": "/PortfolioReact/static/js/main.4f825368.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/PortfolioReact/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/PortfolioReact/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/PortfolioReact/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "12eb2bafb5d16ed9bb22d93b54afe872",
+    "url": "/PortfolioReact/static/media/mount-kilimanjaro.12eb2baf.jpg"
+  },
+  {
+    "revision": "d1375858a641f010f588",
+    "url": "/PortfolioReact/static/css/2.4656b72a.chunk.css"
+  },
+  {
+    "revision": "712e1293239744d855fa66f354f0ca78",
+    "url": "/PortfolioReact/index.html"
+  }
+];
